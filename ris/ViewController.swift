f16f89261d2088a7ce2575f9872e6f5205001af9
nagamiya <appngmy@gmail.com>
2020-04-30 23:48:13 +0900

    Initial Commit

---
+++ ris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ris
+//
+//  Created by nagamiya on 2020/04/30.
+//  Copyright © 2020 nagamiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
